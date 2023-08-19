03c0d2c79028fe90de627c5cbb05f669d0f7d6dd
DEdmonds22 <ddedmonds22@gmail.com>
2023-08-19 19:29:34 -0400

    index.jsx view rendered at pokemon route

---
+++ views/Index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Index({pokemon}) {
+const divStyle = {
+    backgroundColor: 'red'
+}
+
+    const titleStyle = {
+        color: '#ffffff',
+        backgroundColor: '#000000'
+    };
+
+    const linkStyle = {
+        textDecoration: 'none',
+        fontFamily: 'Monaco'
+    }
+
+    return (
+        <div style={divStyle}>
+            <h1 style={titleStyle}>See All The Pokemon</h1>
+            <ul>
+                {pokemon.map((choice, i) => {
+                    return (
+                        <li key={i} style={linkStyle}><img src={choice.img} alt={choice.name} /><br /> {choice.name}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+module.exports = Index;
